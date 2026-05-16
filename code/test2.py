0a413dea6ff60e9e34f458c55bf790de639002fc
Matrix <mateuszkuczerowski832@gmail.com>
2026-05-16 17:10:02 +0200

    Poprawa test2

--- code/test2.py
+++ code/test2.py
@@ -2,60 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import kinematicTools as KT
 from kinematicTools import robot6DOF
-# Generowanie macierzy transformacji ze standardowych parametrów DH
-def dh_matrix(theta, d, a, alpha):
-    return np.array([
-        [np.cos(theta), -np.sin(theta)*np.cos(alpha),  np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
-        [np.sin(theta),  np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
-        [0,              np.sin(alpha),                np.cos(alpha),               d],
-        [0,              0,                            0,                           1]
-    ])
+import mathTools as mt
 
-# Twoje parametry
 L_0, L_1, L_2, L_3, L_4, L_5 = 0.15, 0.20, 0.15, 0.15, 0.15, 0.05
 
-# Przykładowe 6 kątów (q1..q6) do przetestowania (w radianach)
-
-X = 0.2
-Y = 0.2
-Z = 0.1
+X = 0.0
+Y = 0.6
+Z = 0.05
 roll = 0.0
 pitch = 180.0
 yaw = 0.0
 
 robot = robot6DOF(L_0, L_1, L_2, L_3, L_4, L_5)
-q = robot.inverseKinematic(X,Y,Z,roll,pitch,yaw)
+phi_list = robot.inverseKinematic(X,Y,Z,roll,pitch,yaw)
 
-# Tabela DH w kolejności: [theta, d, a, alpha]
 dh_params = [
-    [q[0], L_0,             0,    np.pi/2],
-    [q[1], 0,               L_1,  0],
-    [q[2], 0,               0,   -np.pi/2],
-    [q[3], L_2 + L_3,       0,    np.pi/2],
-    [q[4], 0,               0,   -np.pi/2],
-    [q[5], L_4 + L_5,       0,    0]
+    [phi_list[0], L_0,             0,    np.pi/2],
+    [phi_list[1], 0,               L_1,  0],
+    [phi_list[2], 0,               0,   -np.pi/2],
+    [phi_list[3], L_2 + L_3,       0,    np.pi/2],
+    [phi_list[4], 0,               0,   -np.pi/2],
+    [phi_list[5], L_4 + L_5,       0,    0]
 ]
 
-# Obliczanie pozycji (kinematyka prosta)
-points = [np.array([0, 0, 0])] # Start bazy
-T = np.eye(4)
+points = [np.array([0,0,0])] #punkty startowe
 
+T = np.eye(4)
 for params in dh_params:
-    T = T @ dh_matrix(*params)
-    points.append(T[:3, 3])
+    T = T @ mt.DHmatrix(*params)
+    points.append(T[:3,3])
 
 points = np.array(points)
-xs, ys, zs = points[:, 0], points[:, 1], points[:, 2]
+xs, ys, zs = points[:,0], points[:,1], points[:,2]
 
-# Rysowanie wykresu 3D
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111,projection='3d')
+ax.plot(xs,ys,zs,marker='o',linewidth = 4, markersize = 8, color = 'k')
 
-# Rysowanie linii robota i węzłów
-ax.plot(xs, ys, zs, marker='o', linewidth=4, markersize=8, color='b')
-
-# Formatowanie osi (skala dostosowana do długości członów)
-lim = 0.5
+lim = 0.8
 ax.set_xlim([-lim, lim])
 ax.set_ylim([-lim, lim])
 ax.set_zlim([0, lim])
