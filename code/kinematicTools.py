e08d9ff1fe5df02e298c5c6e2c8384946d05e6bd
Matrix <mateuszkuczerowski832@gmail.com>
2026-07-04 12:54:50 +0200

    Updated class: robot6DOF

--- code/kinematicTools.py
+++ code/kinematicTools.py
@@ -5,7 +5,7 @@ import numpy as np
 class robot6DOF:
 
         """
-        Klasa służaca do symulacji ramienia robota o konkretnej konstrukcji otrzymanej z pomoca metody DH
+        A class used to simulate a robot arm with a specific design derived using the DH method.
         """
 
         def __init__(self,L_0, L_1, L_2, L_3, L_4, L_5):
@@ -16,14 +16,15 @@ class robot6DOF:
                 self.L_4 = L_4
                 self.L_5 = L_5
 
-        def inverseKinematic(self,X,Y,Z,roll,pitch,yaw):
+        def inverseKinematic(self,X,Y,Z,roll,pitch,yaw, elbow_pos:str = 'up', show_angels = True):
 
                 """
-                Funkcja do obliczenia wartości kątowych złączy na podstawie
-                podanej pozycji efektora i oreintacji względem układu bazowego
-
-                Wyjście:
-                kąty złączy w radianach
+                Function for calculating joint angle values ​​based on
+                the given end-effector position and orientation relative to the base frame
+                
+                Input:
+                X, Y, Z - system coordinates in meteres
+                Angels of joints in radians
                 """
 
                 X = X
@@ -41,7 +42,7 @@ class robot6DOF:
                 #phi_0
                 phi_0 = np.atan2(P_wc[1],P_wc[0])
 
-                #zmienne pomocnicze
+                #auxiliary vwariables
                 x = P_wc[0]
                 y = P_wc[1]
 
@@ -50,15 +51,17 @@ class robot6DOF:
                 L_23 = self.L_2 + self.L_3
                 D = (self.L_1**2 + L_23**2 - R**2 - z**2)/(2*self.L_1*L_23)
 
-                #phi_2 +/- zalezy od polozenia łokcia (- dla łokci aw górę)
-                phi_2 = np.atan2(D,-np.sqrt(1-D**2))
-
+                #phi_2 +/- depends on elbow position (- for elbow up)
+                if elbow_pos == 'up':
+                        phi_2 = np.atan2(D,-np.sqrt(1-D**2))
+                else:
+                        phi_2 = np.atan2(D,np.sqrt(1-D**2))
                 #phi_1
                 k1 = self.L_1 - L_23*np.sin(phi_2)
                 k2 = L_23*np.cos(phi_2)
                 phi_1 = np.atan2(k1*z - k2*R, k1*R + k2*z)
 
-                #rotacja 0-3
+                #rotation of joint 0 to 3 
                 mat_rot_0_1 = np.array([
                         [np.cos(phi_0), 0,  np.sin(phi_0),0],
                         [np.sin(phi_0), 0, -np.cos(phi_0),0],
@@ -88,25 +91,26 @@ class robot6DOF:
                 sin_phi4 = np.sqrt(R_3_6[0, 2]**2 + R_3_6[1, 2]**2)
                 cos_phi4 = R_3_6[2, 2]
 
-                # Jeśli nadgarstek jest wyprostowany (osobliwość)
+                # If the wrist is extended (a distinctive feature)
                 if sin_phi4 < 1e-6:
                         phi_4 = 0.0
                         phi_3 = 0.0
-                        # phi_5 przejmuje całkowity obrót nadgarstka
+                        # phi_5 takes over the full rotation of the wrist
                         phi_5 = np.atan2(R_3_6[1, 0], R_3_6[0, 0])
                 else:
-                        # Standardowe wyliczenie
+                        # Standard compute
                         phi_4 = np.atan2(sin_phi4, cos_phi4)
                         phi_3 = np.atan2(-R_3_6[1, 2], -R_3_6[0, 2])
                         phi_5 = np.atan2(-R_3_6[2, 1], R_3_6[2, 0])
-
-                print(f'\nPozycja celu: x: {X}m, y: {Y}, z: {Z}\nroll: {r}, pitch: {p}, yaw: {yaw}')
-                print(f'Phi_0 = {np.degrees(phi_0):.3f}')
-                print(f'Phi_1 = {np.degrees(phi_1):.3f}')
-                print(f'Phi_2 = {np.degrees(phi_2):.3f}')
-                print(f'Phi_3 = {np.degrees(phi_3):.3f}')
-                print(f'Phi_4 = {np.degrees(phi_4):.3f}')
-                print(f'Phi_5 = {np.degrees(phi_5):.3f}')
+                
+                if show_angels:
+                        print(f'\nGoal position: x: {X}m, y: {Y}, z: {Z}\nroll: {r}, pitch: {p}, yaw: {yaw}')
+                        print(f'Phi_0 = {np.degrees(phi_0):.3f}')
+                        print(f'Phi_1 = {np.degrees(phi_1):.3f}')
+                        print(f'Phi_2 = {np.degrees(phi_2):.3f}')
+                        print(f'Phi_3 = {np.degrees(phi_3):.3f}')
+                        print(f'Phi_4 = {np.degrees(phi_4):.3f}')
+                        print(f'Phi_5 = {np.degrees(phi_5):.3f}')
 
                 return phi_0, phi_1, phi_2, phi_3, phi_4, phi_5
 
